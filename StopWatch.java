eaab0f3ad46fb22bf62eb9998b1e3da2e7d85a47
nattokun <avroeum@gmail.com>
2016-11-29 17:47:36 -0500

    Update StopWatch.java

--- StopWatch.java
+++ StopWatch.java
@@ -3,8 +3,24 @@ public class Stopwatch{
   private long stopTime;
   private long elapedTime;
   
-  public void start(){
+  public Stopwatch(){
+    reset();
+  }
   
+  public void reset(){
+    this.elapedTime = 0;
+    this.startTime = 0;
+    this.stopTime = 0;
+  }
+  
+  public void start(){
+    this.startTime = System.currentTimeMillis();
+    
+  }
+  
+  public void stop(){
+    this.stopTime = System.currentTimeMillis();
+    this.elapedTime = (stopTime - this.startTime);
   }
 
 }
